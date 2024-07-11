9508cebf280b2a50f74b531212716dd23e81b474
Samuel Chen <sche842@gmail.com>
2024-07-11 16:24:39 +1000

    projects update

--- src/components/Projects/Portfolio.jsx
+++ src/components/Projects/Portfolio.jsx
@@ -9,18 +9,21 @@ const Portfolio = () => {
         <h2 className="text-col2 text-4xl font-bold">Projects</h2>
         <br />
         <div className="flex gap-8 flex-wrap">
-          {projects.map(({ title, desc, projects, link, code, src }) => (
-            <div key={title}>
-              <Project
-                title={title}
-                desc={desc}
-                projects={projects}
-                link={link}
-                code={code}
-                src={src}
-              />
-            </div>
-          ))}
+          {projects.map(
+            ({ title, desc, projects, link, code, src, skillSet }) => (
+              <div key={title}>
+                <Project
+                  title={title}
+                  desc={desc}
+                  projects={projects}
+                  link={link}
+                  code={code}
+                  src={src}
+                  skillSet={skillSet}
+                />
+              </div>
+            )
+          )}
         </div>
       </div>
     </div>
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Project = ({ title, desc, project, link, code, src }) => {
+const Project = ({ title, desc, project, link, code, src, skillSet }) => {
+  const [click, setClick] = useState(false);
+
   return (
-    <div className="bg-col1 rounded-md overflow-hidden text-white">
-      <div className="py-2 px-2">
-        <img className="rounded-md" src={src} />
+    <div className="bg-col1 rounded-md overflow-hidden text-white px-2 py-2 shadow-md shadow-white">
+      <div className="">
+        <img className="rounded-md" src={src} alt={`${title} image`} />
       </div>
       <p className="py-2 text-center">{title}</p>
       <div className="">
         <p>{desc}</p>
-        <div className="flex justify-between">
+        <div className="flex justify-center items-center gap-4">
+          {skillSet.map((skill) => (
+            <div className="bg-blue-500 py-1 px-2 rounded-full">{skill}</div>
+          ))}
+        </div>
+        <div className="flex justify-center gap-4">
           <a
             href={link}
             target="_blank"
@@ -24,7 +31,6 @@ const Project = ({ title, desc, project, link, code, src }) => {
           >
             code
           </a>
-          <button>read more</button>
         </div>
       </div>
     </div>
