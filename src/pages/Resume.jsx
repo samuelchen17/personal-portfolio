e2b30d7705eb0f83c71f9547371006cc6dc17e05
Samuel Chen <sche842@gmail.com>
2024-07-17 23:59:58 +1000

    resume download test

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,8 +3,10 @@ import NavBar from "../components/Navbar/NavBar";
 import NavModal from "../components/Navbar/NavModal";
 import { AnimatePresence } from "framer-motion";
 import resume from "../../public/resume/resume.jpg";
+import resumePdf from "../../public/resume/resume.pdf";
 import { motion } from "framer-motion";
 import { MdDownload } from "react-icons/md";
+import { PiDnaFill } from "react-icons/pi";
 
 const Resume = () => {
   const [navModal, setNavModal] = useState(false);
@@ -15,15 +17,17 @@ const Resume = () => {
         {navModal && <NavModal setNavModal={setNavModal} navModal={navModal} />}
       </AnimatePresence>
       <div className="h-20 bg-colbg" />
-
-      <div className="flex flex-col justify-center items-center h-screen bg-[url(assets/background/heroBg.jpg)] bg-cover bg-center">
-        <motion.button
-          className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11 flex items-center justify-center"
+      <div className="flex flex-col justify-center items-center bg-[url(assets/background/heroBg.jpg)] bg-cover bg-center min-h-svh">
+        <motion.a
+          className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11 flex items-center justify-center my-6"
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
+          href={resumePdf}
+          download={true}
+          target="_blank"
         >
           download <MdDownload size={25} />
-        </motion.button>
+        </motion.a>
 
         <img src={resume} />
       </div>
