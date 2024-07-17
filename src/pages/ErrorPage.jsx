e2c1b8e36d37c81705b52db3fc6ea975e6a26edc
Samuel Chen <sche842@gmail.com>
2024-07-18 00:30:10 +1000

    404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import NavBar from "../components/Navbar/NavBar";
+import NavModal from "../components/Navbar/NavModal";
+import Footer from "../components/Footer";
+import { AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const ErrorPage = () => {
+  const [navModal, setNavModal] = useState(false);
   return (
-    <div>
-      <div>404 Not Found</div>
-      <Link to="/">
-        <button className="bg-blue-500">Home</button>
-      </Link>
+    <div className="h-dvh bg-col12 text-white">
+      <NavBar navModal={navModal} setNavModal={setNavModal} />
+      <AnimatePresence initial={false} onExitComplete={() => null}>
+        {navModal && <NavModal setNavModal={setNavModal} navModal={navModal} />}
+      </AnimatePresence>
+      <div className="h-20 bg-colbg" />
+
+      <div className="py-4 px-4 mx-auto flex flex-col gap-4 max-w-screen-lg">
+        <h2 className="text-col2 text-9xl font-bold">404</h2>
+        <p className="text-2xl">Page Not Found 😔</p>
+        <Link to="/">
+          <motion.button
+            className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Home
+          </motion.button>
+        </Link>
+      </div>
+      <div className="fixed bottom-0 w-full">
+        <Footer />
+      </div>
     </div>
   );
 };
