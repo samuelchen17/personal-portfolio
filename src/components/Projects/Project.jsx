dba47ee4ab277e562f65442586c27d83a77b804c
Samuel Chen <sche842@gmail.com>
2024-07-17 23:21:21 +1000

    ui update

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -6,59 +6,58 @@ const Project = ({ title, desc, link, code, src, skillSet }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <motion.div
-      className="bg-gray-800 rounded-md overflow-hidden text-white"
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      <div className="w-full overflow-hidden">
-        <img
-          className="w-full h-full object-cover"
-          src={src}
-          alt={`${title} image`}
-        />
-      </div>
+    <div className="flex justify-center items-center cursor-pointer">
+      <motion.div
+        className="bg-gray-800 rounded-md text-white"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="w-full overflow-hidden">
+          <img className="object-cover" src={src} alt={`${title} image`} />
+        </div>
 
-      {isOpen && (
-        <motion.div className="flex flex-col justify-between h-full gap-3 p-4">
-          <p className="text-center text-xl font-bold">{title}</p>
-          <p>{desc}</p>
+        <p className="text-center text-xl font-bold">{title}</p>
 
-          <div className="flex justify-center items-center gap-2 flex-wrap">
-            {skillSet.map((skill, index) => (
-              <div
-                key={index}
-                className="bg-col11 py-1 px-2 rounded-full text-sm"
+        {isOpen && (
+          <motion.div className="flex flex-col gap-3 p-4 h-full">
+            <p>{desc}</p>
+
+            <div className="flex justify-center items-center gap-2 flex-wrap">
+              {skillSet.map((skill, index) => (
+                <div
+                  key={index}
+                  className="bg-col11 py-1 px-2 rounded-full text-sm"
+                >
+                  {skill}
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-center gap-4">
+              <motion.a
+                href={link}
+                target="_blank"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11 flex"
               >
-                {skill}
-              </div>
-            ))}
-          </div>
-          <div className="flex justify-center gap-4">
-            <motion.a
-              href={link}
-              target="_blank"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11 flex"
-            >
-              Demo
-              <MdArrowRight className="" size={25} />
-            </motion.a>
+                Demo
+                <MdArrowRight className="" size={25} />
+              </motion.a>
 
-            <motion.a
-              href={code}
-              target="_blank"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11 flex"
-            >
-              CODE
-              <MdArrowRight className="" size={25} />
-            </motion.a>
-          </div>
-        </motion.div>
-      )}
-    </motion.div>
+              <motion.a
+                href={code}
+                target="_blank"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11 flex"
+              >
+                CODE
+                <MdArrowRight className="" size={25} />
+              </motion.a>
+            </div>
+          </motion.div>
+        )}
+      </motion.div>
+    </div>
   );
 };
 
