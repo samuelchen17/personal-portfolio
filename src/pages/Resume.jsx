587d70573975c851fda8d3f25a5723ed05fa3699
Samuel Chen <sche842@gmail.com>
2024-07-18 00:02:45 +1000

    resume page prototype

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,7 +6,7 @@ import resume from "../../public/resume/resume.jpg";
 import resumePdf from "../../public/resume/resume.pdf";
 import { motion } from "framer-motion";
 import { MdDownload } from "react-icons/md";
-import { PiDnaFill } from "react-icons/pi";
+import Footer from "../components/Footer";
 
 const Resume = () => {
   const [navModal, setNavModal] = useState(false);
@@ -17,7 +17,7 @@ const Resume = () => {
         {navModal && <NavModal setNavModal={setNavModal} navModal={navModal} />}
       </AnimatePresence>
       <div className="h-20 bg-colbg" />
-      <div className="flex flex-col justify-center items-center bg-[url(assets/background/heroBg.jpg)] bg-cover bg-center min-h-svh">
+      <div className="flex flex-col justify-center items-center bg-[url(assets/background/heroBg.jpg)] bg-cover bg-center min-h-svh py-16">
         <motion.a
           className="bg-col2 py-2 px-4 rounded-md font-semibold uppercase text-white shadow-md hover:bg-col11 flex items-center justify-center my-6"
           whileHover={{ scale: 1.1 }}
@@ -29,8 +29,9 @@ const Resume = () => {
           download <MdDownload size={25} />
         </motion.a>
 
-        <img src={resume} />
+        <img src={resume} className="w-full max-w-6xl" />
       </div>
+      <Footer />
     </div>
   );
 };
